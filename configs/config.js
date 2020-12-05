5b2ef9bf2e71181c4b539f12e10e3439c94ce8ee
Iqdam Musayyad <iamusayyad@gmail.com>
2020-12-06 01:29:07 +0700

    Did something

--- configs/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 3000,
-  URL: process.env.BASE_URL || "http://localhost:3000",
-  JWT_SECRET: process.env.JWT_SECRET || "secret0",
-};
