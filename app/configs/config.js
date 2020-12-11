e5140feaec48226f2864cffda3b38a9d89fb1ff6
Iqdam Musayyad <iamusayyad@gmail.com>
2020-12-11 13:32:46 +0700

    Fix config

--- app/configs/config.js
+++ app/configs/config.js
@@ -8,8 +8,8 @@ dotenv.config({
 module.exports = {
   app: {
     NAME: process.env.APP_NAME,
-    HOST: process.env.APP_HOST,
-    PORT: process.env.APP_PORT,
+    HOST: process.env.APP_HOST || process.env.HOST,
+    PORT: process.env.APP_PORT || process.env.PORT,
     ENV: process.env.NODE_ENV,
     SECRET_KEY: process.env.APP_SECRET_KEY,
   },
